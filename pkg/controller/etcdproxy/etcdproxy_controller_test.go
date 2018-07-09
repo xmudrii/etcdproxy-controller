302df6a835d1e5065cd9f076d615cf904082a2c4
Marko Mudrinić <mudrinic.mare@gmail.com>
2018-07-09 02:14:48 +0200

    EtcdStorage Controller: Add Unit tests for etcd-proxy certificates handling

--- pkg/controller/etcdproxy/etcdproxy_controller_test.go
+++ pkg/controller/etcdproxy/etcdproxy_controller_test.go
@@ -1,33 +1,73 @@
 package etcdproxy
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/xmudrii/etcdproxy-controller/pkg/apis/etcd/v1alpha1"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kubeclient "k8s.io/client-go/kubernetes/fake"
-	rslisters "k8s.io/client-go/listers/apps/v1"
-	svclisters "k8s.io/client-go/listers/core/v1"
+	"reflect"
 
 	etcdclient "github.com/xmudrii/etcdproxy-controller/pkg/client/clientset/versioned/fake"
 	etcdlisters "github.com/xmudrii/etcdproxy-controller/pkg/client/listers/etcd/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubeclient "k8s.io/client-go/kubernetes/fake"
+	rslisters "k8s.io/client-go/listers/apps/v1"
+	corelisters "k8s.io/client-go/listers/core/v1"
 )
 
 func TestSyncHandler(t *testing.T) {
 	etcdStorage := func(name string) *v1alpha1.EtcdStorage {
 		return &v1alpha1.EtcdStorage{
 			ObjectMeta: metav1.ObjectMeta{Name: name},
+			Spec: v1alpha1.EtdcStorageSpec{
+				CACertConfigMaps: []v1alpha1.CABundleDestination{
+					{
+						Name:      "etcd-serving-ca",
+						Namespace: "k8s-sample-apiserver",
+					},
+				},
+				ClientCertSecrets: []v1alpha1.ClientCertificateDestination{
+					{
+						Name:      "etcd-client-cert",
+						Namespace: "k8s-sample-apiserver",
+					},
+				},
+			},
+		}
+	}
+	etcdStorageNoCerts := func(name string) *v1alpha1.EtcdStorage {
+		return &v1alpha1.EtcdStorage{
+			ObjectMeta: metav1.ObjectMeta{Name: name},
+		}
+	}
+	configMap := func(name, namespace string) *v1.ConfigMap {
+		return &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+		}
+	}
+	secret := func(name, namespace string) *v1.Secret {
+		return &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
 		}
 	}
 
 	tests := []struct {
 		name                   string
 		startingEtcdStorage    *v1alpha1.EtcdStorage
+		startingConfigMap      *v1.ConfigMap
+		startingSecret         *v1.Secret
 		etcdProxyConfig        *EtcdProxyControllerConfig
 		expectedReplicaSetName string
 		expectedServiceName    string
@@ -35,6 +75,8 @@ func TestSyncHandler(t *testing.T) {
 		{
 			name:                "test simple creation",
 			startingEtcdStorage: etcdStorage("test-1"),
+			startingConfigMap:   configMap("etcd-serving-ca", "k8s-sample-apiserver"),
+			startingSecret:      secret("etcd-client-cert", "k8s-sample-apiserver"),
 			etcdProxyConfig: &EtcdProxyControllerConfig{
 				CoreEtcd: &CoreEtcdConfig{
 					URLs:            []string{"https://test.etcd.svc:2379"},
@@ -50,6 +92,8 @@ func TestSyncHandler(t *testing.T) {
 		{
 			name:                "test simple creation with non-default namespace",
 			startingEtcdStorage: etcdStorage("test-2"),
+			startingConfigMap:   configMap("etcd-serving-ca", "k8s-sample-apiserver"),
+			startingSecret:      secret("etcd-client-cert", "k8s-sample-apiserver"),
 			etcdProxyConfig: &EtcdProxyControllerConfig{
 				CoreEtcd: &CoreEtcdConfig{
 					URLs:            []string{"https://test.etcd.svc:2379"},
@@ -62,6 +106,21 @@ func TestSyncHandler(t *testing.T) {
 			expectedReplicaSetName: "etcd-rs-test-2",
 			expectedServiceName:    "etcd-test-2",
 		},
+		{
+			name:                "test simple creation without apiserver configmap and secret provided in spec",
+			startingEtcdStorage: etcdStorageNoCerts("test-3"),
+			etcdProxyConfig: &EtcdProxyControllerConfig{
+				CoreEtcd: &CoreEtcdConfig{
+					URLs:            []string{"https://test.etcd.svc:2379"},
+					CAConfigMapName: "etcd-coreserving-ca",
+					CertSecretName:  "etcd-coreserving-cert",
+				},
+				ControllerNamespace: "test-storage",
+				ProxyImage:          "quay.io/coreos/etcd:v3.2.18",
+			},
+			expectedReplicaSetName: "etcd-rs-test-3",
+			expectedServiceName:    "etcd-test-3",
+		},
 	}
 
 	for _, tc := range tests {
@@ -69,6 +128,15 @@ func TestSyncHandler(t *testing.T) {
 			indexer := cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, cache.Indexers{})
 			etcdObjs := []runtime.Object{}
 			objs := []runtime.Object{}
+
+			if tc.startingConfigMap != nil {
+				objs = append(objs, tc.startingConfigMap)
+				indexer.Add(tc.startingConfigMap)
+			}
+			if tc.startingSecret != nil {
+				objs = append(objs, tc.startingSecret)
+				indexer.Add(tc.startingSecret)
+			}
 
 			etcdObjs = append(etcdObjs, tc.startingEtcdStorage)
 			indexer.Add(tc.startingEtcdStorage)
@@ -82,7 +150,7 @@ func TestSyncHandler(t *testing.T) {
 
 				kubeclientset:     kubeClient,
 				replicasetsLister: rslisters.NewReplicaSetLister(indexer),
-				servicesLister:    svclisters.NewServiceLister(indexer),
+				servicesLister:    corelisters.NewServiceLister(indexer),
 				recorder:          &record.FakeRecorder{},
 
 				config: tc.etcdProxyConfig,
@@ -108,6 +176,142 @@ func TestSyncHandler(t *testing.T) {
 			}
 			if err != nil {
 				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestSyncHandlerFailure(t *testing.T) {
+	etcdStorage := func(name string) *v1alpha1.EtcdStorage {
+		return &v1alpha1.EtcdStorage{
+			ObjectMeta: metav1.ObjectMeta{Name: name},
+			Spec: v1alpha1.EtdcStorageSpec{
+				CACertConfigMaps: []v1alpha1.CABundleDestination{
+					{
+						Name:      "etcd-serving-ca",
+						Namespace: "k8s-sample-apiserver",
+					},
+				},
+				ClientCertSecrets: []v1alpha1.ClientCertificateDestination{
+					{
+						Name:      "etcd-client-cert",
+						Namespace: "k8s-sample-apiserver",
+					},
+				},
+			},
+		}
+	}
+	configMap := func(name, namespace string) *v1.ConfigMap {
+		return &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+		}
+	}
+	secret := func(name, namespace string) *v1.Secret {
+		return &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+		}
+	}
+
+	tests := []struct {
+		name                string
+		startingEtcdStorage *v1alpha1.EtcdStorage
+		startingConfigMap   *v1.ConfigMap
+		startingSecret      *v1.Secret
+		etcdProxyConfig     *EtcdProxyControllerConfig
+		expectedErrors      []error
+	}{
+		{
+			name:                "test creation without ca cert configmap created",
+			startingConfigMap:   nil,
+			startingSecret:      secret("etcd-client-cert", "k8s-sample-apiserver"),
+			startingEtcdStorage: etcdStorage("test-1"),
+			etcdProxyConfig: &EtcdProxyControllerConfig{
+				CoreEtcd: &CoreEtcdConfig{
+					URLs:            []string{"https://test.etcd.svc:2379"},
+					CAConfigMapName: "etcd-coreserving-ca",
+					CertSecretName:  "etcd-coreserving-cert",
+				},
+				ControllerNamespace: "kube-apiserver-storage",
+				ProxyImage:          "quay.io/coreos/etcd:v3.2.18",
+			},
+			expectedErrors: []error{fmt.Errorf("configmaps \"etcd-serving-ca\" not found")},
+		},
+		{
+			name:                "test creation without clinet cert secret created",
+			startingEtcdStorage: etcdStorage("test-2"),
+			startingConfigMap:   configMap("etcd-serving-ca", "k8s-sample-apiserver"),
+			startingSecret:      nil,
+			etcdProxyConfig: &EtcdProxyControllerConfig{
+				CoreEtcd: &CoreEtcdConfig{
+					URLs:            []string{"https://test.etcd.svc:2379"},
+					CAConfigMapName: "etcd-coreserving-ca",
+					CertSecretName:  "etcd-coreserving-cert",
+				},
+				ControllerNamespace: "kube-apiserver-storage",
+				ProxyImage:          "quay.io/coreos/etcd:v3.2.18",
+			},
+			expectedErrors: []error{fmt.Errorf("secrets \"etcd-client-cert\" not found")},
+		},
+		{
+			name:                "test creation without ca cert configmap and client cert secret created",
+			startingEtcdStorage: etcdStorage("test-3"),
+			startingConfigMap:   nil,
+			startingSecret:      nil,
+			etcdProxyConfig: &EtcdProxyControllerConfig{
+				CoreEtcd: &CoreEtcdConfig{
+					URLs:            []string{"https://test.etcd.svc:2379"},
+					CAConfigMapName: "etcd-coreserving-ca",
+					CertSecretName:  "etcd-coreserving-cert",
+				},
+				ControllerNamespace: "kube-apiserver-storage",
+				ProxyImage:          "quay.io/coreos/etcd:v3.2.18",
+			},
+			expectedErrors: []error{fmt.Errorf("configmaps \"etcd-serving-ca\" not found"),
+				fmt.Errorf("secrets \"etcd-client-cert\" not found")},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			indexer := cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, cache.Indexers{})
+			etcdObjs := []runtime.Object{}
+			objs := []runtime.Object{}
+
+			if tc.startingConfigMap != nil {
+				objs = append(objs, tc.startingConfigMap)
+				indexer.Add(tc.startingConfigMap)
+			}
+			if tc.startingSecret != nil {
+				objs = append(objs, tc.startingSecret)
+				indexer.Add(tc.startingSecret)
+			}
+
+			etcdObjs = append(etcdObjs, tc.startingEtcdStorage)
+			indexer.Add(tc.startingEtcdStorage)
+
+			etcdstorageClient := etcdclient.NewSimpleClientset(etcdObjs...)
+			kubeClient := kubeclient.NewSimpleClientset(objs...)
+
+			c := EtcdProxyController{
+				etcdProxyClient:    etcdstorageClient,
+				etcdstoragesLister: etcdlisters.NewEtcdStorageLister(indexer),
+
+				kubeclientset:     kubeClient,
+				replicasetsLister: rslisters.NewReplicaSetLister(indexer),
+				servicesLister:    corelisters.NewServiceLister(indexer),
+				recorder:          &record.FakeRecorder{},
+
+				config: tc.etcdProxyConfig,
+			}
+			errs := c.syncHandler(tc.startingEtcdStorage.Name)
+			if reflect.DeepEqual(errs, tc.expectedErrors) {
+				t.Fatalf("expected error(s): '%v',\nbut got error(s): '%v'", tc.expectedErrors, errs)
 			}
 		})
 	}
